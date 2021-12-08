7ee312afac002ba557106b5988b69ea26a093384
theodoreche <2226390@gmail.com>
2021-12-08 22:45:55 +0100

    add breakpoints

---
+++ constants/breakpoints.ts
@@ -0,0 +1,12 @@
+export const size = {
+  sm: '576px',
+  md: '768px',
+  lg: '992px',
+  xl: '1200px',
+}
+export const device = {
+  sm: `(min-width: ${size.sm})`,
+  md: `(min-width: ${size.md})`,
+  lg: `(min-width: ${size.lg})`,
+  xl: `(min-width: ${size.xl})`,
+}
