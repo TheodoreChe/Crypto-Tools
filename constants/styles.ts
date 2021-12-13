db3fef732666a77ee578a58873a43de48cd900a3
theodoreche <2226390@gmail.com>
2021-12-13 21:32:36 +0100

    add styles constants

---
+++ constants/styles.ts
@@ -0,0 +1,9 @@
+import { css } from 'styled-components'
+
+export const actionElement = css`
+  padding: 1rem;
+  border: var(--border);
+  border-radius: var(--radius);
+  font-size: 1rem;
+  line-height: 1.75rem;
+`
