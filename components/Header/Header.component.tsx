f2769cbba73fe2a7c9f556e614b839082b61cdfb
theodoreche <2226390@gmail.com>
2021-12-13 21:29:48 +0100

    add Header component

--- components/Header/Header.component.tsx
+++ components/Header/Header.component.tsx
@@ -2,27 +2,34 @@ import { FC, ReactNode } from 'react'
 import styled from 'styled-components'
 
 interface Props {
-    black?: boolean
-    center?: boolean
-    border?: boolean
+  black?: boolean
+  center?: boolean
+  border?: boolean
 }
 
 const Header = styled.div<Props>`
   display: flex;
   align-items: center;
   height: 6rem;
-  padding: 0 2.5rem;
+  padding: 0 var(--gap);
+  border-radius: var(--radius);
 
-  ${({ black }) => black && `
+  ${({ black }) =>
+    black &&
+    `
     background-color: var(--black);
     color: var(--white);
   `}
 
-  ${({ center }) => center && `
+  ${({ center }) =>
+    center &&
+    `
     justify-content: center;
   `}
 
-  ${({ border }) => border && `
+  ${({ border }) =>
+    border &&
+    `
     border: thin solid var(--black);
   `}
 `
