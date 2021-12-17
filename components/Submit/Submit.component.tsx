46a863e1e4ee8958c8d831544d4e4e1d0b596d80
theodoreche <2226390@gmail.com>
2021-12-17 07:34:52 +0100

    add submit component

---
+++ components/Submit/Submit.component.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+import { actionElement } from "@/constants/styles";
+
+const SubmitComponent = styled.input`
+  ${actionElement};
+  background: transparent;
+  cursor: pointer;
+  padding: 1rem 3.75rem;
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.4);
+  }
+`
+
+export default SubmitComponent
+
+
+
