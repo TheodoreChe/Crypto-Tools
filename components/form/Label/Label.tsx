ce3433b4981e7abf2785950e4cc1333634ef1e59
theodoreche <2226390@gmail.com>
2021-12-27 23:31:50 +0100

    add Label component

---
+++ components/form/Label/Label.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const LabelComponent = styled.label`
+  display: block;
+  position: relative;
+  font-size: 0.875rem;
+  font-weight: bold;
+  margin-bottom: 1.25rem;
+`
+
+export default LabelComponent
