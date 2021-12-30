ef26c1a81ac5aa0d97631b847de96deee110ca2d
theodoreche <2226390@gmail.com>
2021-12-30 19:59:06 +0100

    add favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -28,11 +28,17 @@ class MyDocument extends Document {
   }
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
           <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1+Code:wght@402&display=swap" rel="stylesheet" />
+          <meta name="theme-color" content="#000" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="manifest" href="/site.webmanifest" />
         </Head>
         <body>
           <Main />
