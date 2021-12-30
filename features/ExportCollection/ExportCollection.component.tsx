2d9c6075bd5931c6770d179ad22735ba8c54a967
theodoreche <2226390@gmail.com>
2021-12-30 23:55:18 +0100

    add export collection

---
+++ features/ExportCollection/ExportCollection.component.tsx
@@ -0,0 +1,113 @@
+import React, { FC, useState } from 'react'
+import styled from 'styled-components'
+import Header from '@/components/Header'
+import {
+  getCollectionName,
+  getProperties,
+  getPropertiesCartesianProduct,
+  Option,
+  sliceArrayByChunks,
+} from '@/state/collection'
+import { useAppSelector } from '@/state/hooks'
+import { actionElement } from '@/constants/styles'
+import mergeImages from 'merge-images'
+import localForage from 'localforage'
+import { saveAs } from 'file-saver'
+import JSZip from 'JSZip'
+import mergeOptions from '../../lib/mergeOptions'
+
+const ExportCollectionComponent = styled.div<{ disabled?: boolean }>`
+  height: 0;
+  flex-grow: 1;
+  flex-shrink: 1;
+  overflow: auto;
+  padding: var(--gap);
+  opacity: ${({ disabled }) => (disabled ? '0.5' : '1')};
+`
+
+const Item = styled.div`
+  display: flex;
+  width: 100%;
+  margin-bottom: var(--gap);
+`
+
+const ItemNumber = styled.div`
+  background-color: var(--black);
+  color: var(--white);
+  border: var(--border);
+  border-radius: var(--radius);
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  min-width: 6rem;
+  justify-content: center;
+  margin-right: 1rem;
+`
+
+const ItemDescription = styled.div`
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  flex: 1;
+`
+
+const ItemButton = styled.button`
+  ${actionElement};
+  background: transparent;
+  cursor: pointer;
+  padding: 1rem 3.75rem;
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.4);
+  }
+`
+
+const ExportCollection: FC = () => {
+  const properties = useAppSelector(getProperties)
+  const propertiesCartesianProduct = useAppSelector(getPropertiesCartesianProduct)
+  const collectionName = useAppSelector(getCollectionName) || 'collection'
+  const slicedPropertiesProduct = sliceArrayByChunks(propertiesCartesianProduct)
+  const [inProgress, setInProgress] = useState(false)
+  const exportHandle = (product: any) => async () => {
+    setInProgress(true)
+
+    const zip = new JSZip()
+    const folder = zip.folder(collectionName)
+
+    const blobList = await Promise.all(
+      product.map(async (options: Option[]) => {
+        const base64 = await mergeOptions(options)
+        return await fetch(base64).then((res) => res.blob())
+      }),
+    )
+
+    blobList.map((blob, index) => {
+      folder && folder.file(`${collectionName}__${index}.png`, blob)
+    })
+
+    zip.generateAsync({ type: 'blob' }).then(function (content) {
+      // see FileSaver.js
+      saveAs(content, `${collectionName}.zip`)
+    })
+
+    setInProgress(false)
+  }
+
+  return (
+    <>
+      <Header>
+        <h2>Export Collection</h2>
+      </Header>
+      <ExportCollectionComponent disabled={inProgress}>
+        {slicedPropertiesProduct.map((product: any, index: number) => (
+          <Item key={index}>
+            <ItemNumber>Part {index + 1}</ItemNumber>
+            <ItemButton onClick={exportHandle(product)}>Export</ItemButton>
+            <ItemDescription>{product.length} items</ItemDescription>
+          </Item>
+        ))}
+      </ExportCollectionComponent>
+    </>
+  )
+}
+
+export default ExportCollection
