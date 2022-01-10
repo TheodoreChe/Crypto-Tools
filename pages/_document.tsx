b6051c50f2189de47166ee8702a7e88bbe32c4d2
theodoreche <2226390@gmail.com>
2022-01-10 22:19:16 +0100

    edit fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -32,10 +32,7 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=M+PLUS+1+Code:wght@400;500;600&display=swap"
-            rel="stylesheet"
-          />
+          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet" />
           <meta name="theme-color" content="#000" />
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
