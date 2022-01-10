824cda75cd7e4a2ea5d398da6f461d4bd69ac7ba
theodoreche <2226390@gmail.com>
2022-01-10 22:19:37 +0100

    fix mergeOptions

--- lib/mergeOptions.ts
+++ lib/mergeOptions.ts
@@ -4,7 +4,9 @@ import { Option } from '@/state/collection'
 
 export default async function mergeOptions(optionList: Option[]) {
   const getBlobList = optionList.map(({ fileName }) => fileName && localForage.getItem<Blob>(fileName))
-  const blobList = (await Promise.all(getBlobList)) as Blob[]
-  const urlList = blobList.map(URL.createObjectURL)
+  const blobList = await Promise.all(getBlobList)
+  const safeBlobList = blobList.filter(Boolean) as Blob[]
+
+  const urlList = safeBlobList.map(URL.createObjectURL)
   return mergeImages(urlList)
 }
